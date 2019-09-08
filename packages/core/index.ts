20bcaaa34b839aadcd5e1e52ec9732b06534ac49
supergrecko <me@supergrecko.com>
2019-09-08 14:30:56 +0200

    chore: Move git to own repo

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -1,6 +1,7 @@
 import { task } from './src/core/Task'
 import { Command } from './src/App'
 import { Plugin } from './src/App'
+import { Tasky as TaskyApp } from './src/core/Tasky'
 import Tasky from './src/App'
 
 export default Tasky
@@ -8,5 +9,6 @@ export default Tasky
 export {
     task,
     Command,
-    Plugin
+    Plugin,
+    TaskyApp
 }
--- packages/core/src/App.ts
+++ packages/core/src/App.ts
@@ -1,8 +1,4 @@
 import Tasky from './core/Tasky'
-import GitPlugin  from './plugins/Git'
-
-Tasky
-    .use("git", GitPlugin)
 
 export default Tasky
 export { Plugin } from './core/Plugin'
