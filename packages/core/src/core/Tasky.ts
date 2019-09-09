bf3cf110979976321f6884450760ac7b7b57969f
Phi <floyd_spiky@hotmail.com>
2019-09-09 14:12:15 -0400

    Adding git as a hardcoded type for task()

--- packages/core/src/core/Tasky.ts
+++ packages/core/src/core/Tasky.ts
@@ -1,5 +1,11 @@
 import { Plugin } from './Plugin'
 import chalk from 'chalk'
+import { Git } from '@tasky/git'
+
+interface MapPlugin {
+    git: Git
+    [key: string]: any
+}
 
 export class Tasky {
 
@@ -26,6 +32,7 @@ export class Tasky {
      * @param name
      * @param callback
      */
+    public task<T extends MapPlugin[K], K extends keyof MapPlugin = any>(name: K, callback: (plugin: T) => void): Tasky;
     public task<T extends Plugin>(name: string, callback: (plugin: T) => void): Tasky {
         const plugin = this.plugins.get(name)
 
